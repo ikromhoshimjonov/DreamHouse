cd2a09fae6127aecbc10bc833766032348a90ccb
ikromhoshimjonov <hoshimjonovikromjon4@gmail.com>
2025-07-11 21:00:54 +0500

    as

--- root/settings.py
+++ root/settings.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
 
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Your Project API',
+    'TITLE': 'PDP Project',
     'DESCRIPTION': 'Your project description',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
